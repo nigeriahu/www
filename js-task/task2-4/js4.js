310a939e32fc0b69c7e5861d7aeab58aa35bcef5
nigeriahu <nigeriahu@163.com>
2016-12-21 17:27:13 +0800

    12-21

--- js-task/task2-4/js4.js
+++ js-task/task2-4/js4.js
@@ -8,3 +8,15 @@ all = JSON.parse(allArr);
 console.log(all);
 
 console.log(allArr+"长度"+all.length);
+console.log(allArr+"长度"+all.length);
+var main=$('#box');
+for(i=0;i<all.length;i++){
+    var player=document.createElement('div');
+    player.className='box-top';
+    player.innerHTML ="<p>"+ "<img src='putao.png'>"+all[i]+"</p> <span>"+(i+1)+"</span>";
+    main.append(player);
+    console.log(all[i]);
+}
+function confirm() {
+    location.href="js4-1.html";
+}
