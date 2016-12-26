b5ec2d83c450db433780fe11742e5636824d8528
nigeriahu <nigeriahu@163.com>
2016-12-26 16:34:14 +0800

    12-26

--- js-task/task2-4/js4-2.js
+++ js-task/task2-4/js4-2.js
@@ -2,8 +2,35 @@
  * Created by michael on 2016/12/23.
  */
 
+    // 读取本地缓存中的数组；
+console.log(localStorage);
+allArr=localStorage.all;
+all=JSON.parse(allArr);
+console.log(all)
+console.log("玩家身份点击之后"+all[click].status);
 
+KillNString=localStorage.kill;
+KillN=JSON.parse(KillNString);
+localStorage.KillN=KillNS;
+dayString=localStorage.day;
+day=JSON.parse(dayString);
+var voteN;
 
-function start() {
-    location.href="js4.html";
+if (localStorage.vote) {
+    voteString=localStorage.vote;
+    voteN=JSON.parse(voteString);
 }
+
+for (var i = 0; i < day; i++) {
+    var checkId=$('#result');
+    ali='<p>'+(all[killN[i]].num+1)+'号被杀手杀死了，真实身份是'+all[killN[i]].job+'</p>';
+    checkId.append(ali);
+    if (voteN!==undefined&&voteN.length>i) {
+        oli='<p>'+(all[voteN[i]].num+1)+'号被集体投死了，真实身份是'+all[voteN[i]].id+'</p>';
+        checkId.append(oli);
+    }
+}
+$('.footer button').on('click',function () {
+    location.href='js4.html';
+})
+
--- js-task/task2-4/js4.js
+++ js-task/task2-4/js4.js
@@ -7,9 +7,23 @@ allArr = sessionStorage.all;
 all = JSON.parse(allArr);
 console.log(all);
 
-var playerStatus=[];//空数组存储玩家对象的状态
+var playerStatus=[];//空数组存储玩家对象的状态对应killN？
+var click;
+var killN=[];
 
-console.log(allArr+"长度"+all.length);
+// 删除数组中的第一个
+//all.shift();
+
+// 读取天数
+if(localStorage.day){
+    dayString=localStorage.day;
+    day=JSON.parse(dayString);
+}
+//读取已被杀的数据
+if (localStorage.kill) {
+    KillNString=localStorage.kill;
+    KillN=JSON.parse(KillNString);
+}
 console.log(allArr+"长度"+all.length);
 var main=$('#box');
 for(i=0;i<all.length;i++){
@@ -17,22 +31,42 @@ for(i=0;i<all.length;i++){
     player.className='box-top';
     player.innerHTML ="<p>"+ "<img src='putao.png'>"+all[i]+"</p> <span>"+(i+1)+"</span>";
     main.append(player);
-    playerStatus[i]= {};
+    playerStatus[i]= {};    //此处空数组为对象，存储all数组的属性，num为对应数字，job为角色，etc
     playerStatus[i].num = i+1;
-    playerStatus[i].job = all[i];
-    playerStatus[i].status = "alive";
-    playerStatus[i].daynum = 0;
-    console.log(all[i]);
-    console.log(playerStatus[i]);
+    playerStatus[i].job = all[i];    //玩家扮演角色
+    playerStatus[i].status = "alive";  //角色生死状态
+    playerStatus[i].daynum = 0;    //杀手杀人执行天数
+    console.log(all[i]);      //打印数列的所有属性
+    console.log("玩家身份"+playerStatus[i].status);
 }
 
 //点击杀人
-// 循环出所有人的身份，死亡的人边框变红
 var box=$('.box-top');
+box.on('click',function () {
+    // 点击边框换色
+    if ($(this).find('.job').html()=='杀手') {
+        alert('为何要手足相残啊，少年！？杀手不能杀死杀手！');
+        click=undefined;
+        $('.box-top').css('border','3px solid #fff')
+    }else if($(this).hasClass('die')){
+        alert('玩家已死 请选择一个没有死亡的玩家！')
+        click=undefined;
+        $('.box-top').css('border','3px solid #fff')
+    }else{
+        $(this).css('border','3px solid orange').siblings().css('border','3px solid #fff');
+        click=$(this).index();
+    }
+    // 重新标记死亡角色；
+    $('.die').css('border','3px solid red')
+})
+console.log(all)
+// 循环出所有人的身份，死亡的人边框变红
+
 for (var i = 0; i < all.length; i++) {
     if (all[i].status=='dead') {
         box.eq(i).css('border','3px solid red').addClass('die');
-    }else if(all[i].status=='vote'){
+       }
+    if(all[i].status=='vote'){
         box.eq(i).css('border','3px solid red').addClass('die');
     }
 }
@@ -43,24 +77,27 @@ box.on('click',function () {
         click=undefined;
         console.log(click)
     }else{
-        $(this).css('border','3px solid white').siblings().css('border','3px solid #fff');
+        $(this).css('border','3px solid red').siblings().css('border','3px solid #fff');
         $('.die').css('border','3px solid red');
         click=$(this).index();
         console.log(click)
     }
 })
-$('footer button').on('click',function () {
+$('#check').on('click',function () {
+
     if (click==undefined) {
         alert('请选择一个非死亡玩家')
     }else{
         all[click].status='vote';
         all[click].day=day;
-        voteN.push(click);
-        voteNS=JSON.stringify(voteN);
-        localStorage.vote=voteNS;
-        attArrString=JSON.stringify(TotalArr);
-        localStorage.Total=attArrString;
-        location.href='js4-2.html'
+        KillN.push(click);
+        KillNS=JSON.stringify(killN);
+        localStorage.KillN=KillNS;
+        allArr=JSON.stringify(all);
+        localStorage.all=allArr;
+        location.href="js4-2.html";
+
     }
+    console.log("玩家身份点击之后"+all[click].status);
 
 })
