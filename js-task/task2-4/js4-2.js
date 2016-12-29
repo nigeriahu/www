cde43176baa80e38525c89c711c3aea98ffa09b1
nigeriahu <nigeriahu@163.com>
2016-12-29 16:23:29 +0800

    12-29

--- js-task/task2-4/js4-2.js
+++ js-task/task2-4/js4-2.js
@@ -14,8 +14,9 @@ KillN=JSON.parse(KillNString);
 console.log(KillN);
 dayString=localStorage.day;
 day=JSON.parse(dayString);
-var voteN;
-
+var voteN=[];
+var k;
+var x,y, z;
 if (localStorage.all) {
     allArr = localStorage.all;
     all = JSON.parse(allArr);
@@ -29,7 +30,8 @@ if (localStorage.vote) {
     voteN=JSON.parse(voteString);
 }
 
-for (var i = 0; i < day*2; i++) {
+
+for (var i = 0; i < day; i++) {
     var output=$('#result');
     var idle=$('#idle');
     idle.children("img").attr("src", "img-true.png");
@@ -41,9 +43,65 @@ for (var i = 0; i < day*2; i++) {
         output.append(oli);
     }
 }
+
+var j;
+var DeadNum = [];//挑出已死的玩家存入数组
+for(j = 0;j < total.length;j ++){
+    if(total[j].status == "dead" || total[j].status == "vote"){
+        DeadNum.push(total[j]);
+    }
+}
+console.log(DeadNum);
+var DeadNumStr = JSON.stringify(DeadNum);//已死玩家存入本地
+localStorage.deadnum = DeadNumStr;
+
+
+for( x = 0,y = 0,z = 0;x<total.length;x++){ //设置胜利条件
+    if(total[x].job == "平民" && total[x].status == "alive"){
+        y ++;
+    }
+    else if(total[x].job == "杀手" && total[x].status == "alive"){
+        z ++;
+    }
+}
+if(y == 0){//判断哪方胜利
+    document.getElementById("result").style.display = "none";
+    document.getElementById("check").style.marginTop = "60px";
+    document.getElementById("check").value = "杀手胜利 查看结果";
+    var victor = "killer";
+    localStorage.victor = victor;
+    console.log(victor);
+}
+else if(z == 0){
+    document.getElementById("discus").style.display = "none";
+    document.getElementById("start").style.marginTop = "60px";
+    document.getElementById("start").value = "平民胜利 查看结果";
+    var winner = "people";
+    localStorage.victor = winner;
+    console.log(winner);
+}else { //未分出胜负
+    if(DeadNum.length % 2 == 0){
+        k = (DeadNum.length/2 - 1);
+        document.getElementById("idle").src = "img-true.png";
+        document.getElementById("result").style.display = "none";
+        document.getElementById("check").style.marginTop = "60px";
+        document.getElementById("check").value = "第" + day[k + 1] + "天";
+    }else {
+        document.getElementById("check").value = "去投票";
+    }
+}
+
+
 $('.footer button').on('click',function () {
 
-    location.href='js4.html';
-
+    if( y == 0 || z == 0){ //分出胜负跳至结果页
+        location.href = "#";
+    }else{
+        if(DeadNum.length % 2 == 0){
+            location.href = "js4.html";
+        }else {
+            location.href = "js4-vote.html";
+        }
+    }
 })
 
