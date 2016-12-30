96a9b15b7276525a18d35d0dd8146aeebe6b404e
nigeriahu <nigeriahu@163.com>
2016-12-30 13:14:19 +0800

    12-30

--- js-task/task2-4/js4-2.js
+++ js-task/task2-4/js4-2.js
@@ -15,8 +15,7 @@ console.log(KillN);
 dayString=localStorage.day;
 day=JSON.parse(dayString);
 var voteN=[];
-var k;
-var x,y, z;
+
 if (localStorage.all) {
     allArr = localStorage.all;
     all = JSON.parse(allArr);
@@ -39,7 +38,7 @@ for (var i = 0; i < day; i++) {
     output.append(ali);
     if (voteN!==undefined&&voteN.length>i) {
         idle.children("img").attr("src", "JS_vote.png");
-        oli='<p>'+(total[voteN[i]].num+1)+'号被集体投死了，真实身份是'+total[voteN[i]].job+'</p>';
+        oli='<p>'+(total[voteN[i]].num)+'号被集体投死了，真实身份是'+total[voteN[i]].job+'</p>';
         output.append(oli);
     }
 }
@@ -55,7 +54,8 @@ console.log(DeadNum);
 var DeadNumStr = JSON.stringify(DeadNum);//已死玩家存入本地
 localStorage.deadnum = DeadNumStr;
 
-
+var k = (DeadNum.length/2 - 1);
+var x,y, z;
 for( x = 0,y = 0,z = 0;x<total.length;x++){ //设置胜利条件
     if(total[x].job == "平民" && total[x].status == "alive"){
         y ++;
@@ -66,25 +66,22 @@ for( x = 0,y = 0,z = 0;x<total.length;x++){ //设置胜利条件
 }
 if(y == 0){//判断哪方胜利
     document.getElementById("result").style.display = "none";
-    document.getElementById("check").style.marginTop = "60px";
     document.getElementById("check").value = "杀手胜利 查看结果";
     var victor = "killer";
     localStorage.victor = victor;
     console.log(victor);
 }
 else if(z == 0){
-    document.getElementById("discus").style.display = "none";
-    document.getElementById("start").style.marginTop = "60px";
-    document.getElementById("start").value = "平民胜利 查看结果";
+    document.getElementById("result").style.display = "none";
+    document.getElementById("check").value = "平民胜利 查看结果";
     var winner = "people";
     localStorage.victor = winner;
     console.log(winner);
 }else { //未分出胜负
     if(DeadNum.length % 2 == 0){
-        k = (DeadNum.length/2 - 1);
+
         document.getElementById("idle").src = "img-true.png";
         document.getElementById("result").style.display = "none";
-        document.getElementById("check").style.marginTop = "60px";
         document.getElementById("check").value = "第" + day[k + 1] + "天";
     }else {
         document.getElementById("check").value = "去投票";
@@ -92,7 +89,7 @@ else if(z == 0){
 }
 
 
-$('.footer button').on('click',function () {
+$('.footer input').on('click',function () {
 
     if( y == 0 || z == 0){ //分出胜负跳至结果页
         location.href = "#";
